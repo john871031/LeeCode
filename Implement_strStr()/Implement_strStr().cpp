2f7b661aaeb59075db5248003aabf2e3313acba9
John <48525510+john871031@users.noreply.github.com>
2022-07-06 00:58:30 +0800

    Create Implement_strStr().cpp

---
+++ Implement_strStr()/Implement_strStr().cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int index;
+        index = haystack.find(needle, 0);
+        if(index >= 0) return index;
+        else return index;
+    }
+};
