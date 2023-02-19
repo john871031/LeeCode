06ced4876353b2e1b1b1c79b6f7ec7a90e8f5d44
John <48525510+john871031@users.noreply.github.com>
2023-02-19 21:21:23 +0800

    Create Sort_Colors.cpp

---
+++ Sort_Colors/Sort_Colors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0) red++;
+            else if(nums[i]==1) white++;
+            else blue++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(red!=0){
+                nums[i] = 0;
+                red--;
+            }else if(white!=0){
+                nums[i] = 1;
+                white--;
+            }else{
+                nums[i] = 2;
+                blue--;
+            }
+        }
+    }
+};
