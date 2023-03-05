26f729ca5a10bd1267a38d4e0a2da3e5187dc2da
John <48525510+john871031@users.noreply.github.com>
2023-03-05 20:55:00 +0800

    Update Subsets_II.cpp

--- Subsets_II/Subsets_II.cpp
+++ Subsets_II/Subsets_II.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     void check(vector< vector<int> > &ans, vector<int> &tmp, int n, vector<int> nums){
-        bool flag = true;
+        // bool flag = true;
         // sort(tmp.begin(), tmp.end());
-        for(int i=0;i<ans.size();i++){
-            if(ans[i]==tmp) flag = false;
-        }
-        if(flag) ans.push_back(tmp);
-        // ans.push_back(tmp);
+        // for(int i=0;i<ans.size();i++){
+        //     if(ans[i]==tmp) flag = false;
+        // }
+        // if(flag) ans.push_back(tmp);
+        ans.push_back(tmp);
         if(n==nums.size()){
             return;
         }
         for(int i=n;i<nums.size();i++){
+            if(i>n && nums[i]==nums[i-1]) continue;
             tmp.push_back(nums[i]);
             check(ans, tmp, i+1, nums);
             tmp.pop_back();
