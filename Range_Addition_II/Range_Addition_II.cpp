8b390dd1213eca9c2b79c310db7d52a7b2559afb
John <48525510+john871031@users.noreply.github.com>
2023-04-03 13:16:26 +0800

    Create Range_Addition_II.cpp

---
+++ Range_Addition_II/Range_Addition_II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        int min_m = m;
+        int min_n = n;
+        for(int i=0;i<ops.size();i++){
+            if(ops[i][0]<min_m) min_m = ops[i][0];
+            if(ops[i][1]<min_n) min_n = ops[i][1];
+        }
+        return min_m*min_n;
+    }
+};
