2f91ac222fe278707467bcd76e02e5d95adc703b
John <48525510+john871031@users.noreply.github.com>
2023-04-15 12:21:40 +0800

    Create Word_Break.cpp

---
+++ Word_Break/Word_Break.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1, 0);
+        dp[0] = true;
+        unordered_set<string> st(wordDict.begin(), wordDict.end());
+        for(int i=1;i<=s.size();i++){
+            for(int j=0;j<i;j++){
+                if(dp[j] && st.count(s.substr(j, i-j))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
