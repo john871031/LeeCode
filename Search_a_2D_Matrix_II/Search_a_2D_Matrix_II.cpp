a8f1642435c05df411e2dd90e245ad1f84d10a40
John <48525510+john871031@users.noreply.github.com>
2023-09-29 01:16:15 +0800

    Create Search_a_2D_Matrix_II.cpp

---
+++ Search_a_2D_Matrix_II/Search_a_2D_Matrix_II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int start = 0;
+        int end = matrix[0].size()-1;
+        while(start<matrix.size() && end>=0){
+            if(matrix[start][end]==target) return true;
+            else if(matrix[start][end]<target) start++;
+            else end--;
+        }
+        
+        return false;
+    }
+};
