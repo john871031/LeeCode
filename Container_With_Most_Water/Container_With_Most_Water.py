90881175c08200c7e3a17b8c4be1b023f11d70c7
John <48525510+john871031@users.noreply.github.com>
2023-11-06 16:28:29 +0800

    Create Container_With_Most_Water.py

---
+++ Container_With_Most_Water/Container_With_Most_Water.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(height)-1
+        area = 0
+        while left<right:
+            h = min(height[left], height[right])
+            tmp = (right-left) * h
+            if area<tmp: area = tmp
+            if(height[left]<height[right]): left += 1
+            else: right -= 1
+        
+        return area
