419164092f14ec953b84cff92d5820250ad32923
John <48525510+john871031@users.noreply.github.com>
2023-11-21 13:29:06 +0800

    Create Minimum_Flips_to_Make_a_OR_b_Equal_to_c.cpp

---
+++ Minimum_Flips_to_Make_a_OR_b_Equal_to_c/Minimum_Flips_to_Make_a_OR_b_Equal_to_c.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int ans = 0;
+
+        for(int i=0;i<32;i++){
+            if(c&1){
+                if(!(a&1) && !(b&1)){
+                    ans++;
+                }
+            }else{
+                if(a&1) ans++;
+                if(b&1) ans++;
+            }
+            a = a>>1;
+            b = b>>1;
+            c = c>>1;
+        }
+        return ans;
+    }
+};
