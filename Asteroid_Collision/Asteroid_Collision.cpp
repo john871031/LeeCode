43426fd441b9326e987611867637d43dac133c67
John <48525510+john871031@users.noreply.github.com>
2023-12-03 15:24:14 +0800

    Create Asteroid_Collision.cpp

---
+++ Asteroid_Collision/Asteroid_Collision.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        int len = asteroids.size();
+        vector<int> ans;
+        
+        for(int i=0;i<asteroids.size();i++){
+            if(asteroids[i]>0) ans.push_back(asteroids[i]);
+            else if(ans.empty() || ans.back()<0) ans.push_back(asteroids[i]);
+            else if(ans.back()<=abs(asteroids[i])){
+                if(ans.back()<abs(asteroids[i])) --i;
+                ans.pop_back();
+            }
+        }
+        return ans;
+    }
+};
